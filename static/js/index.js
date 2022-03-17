8675499d285858dccb588199be5b67e55f3642c3
crCoelhos <cristophersaar@gmail.com>
2022-03-17 16:15:40 -0500

    progresso na imprelementação do sistema de rotas

---
+++ static/js/index.js
@@ -0,0 +1,32 @@
+function initMap(){
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    const directionsService = new google.maps.DirectionsService();
+    const map = new google.maps.Map(document.getElementById("map"),{
+        zoom: 16.3,
+        center: { lat: -9.953317514546832, lng:-67.86271998458022 },
+    });
+
+    directionsRenderer.setMap(map)
+    calcularRota(directionsService, directionsRenderer);
+    document.getElementById("mode").addEventListener("change", () => {
+        calcularRota(directionsService, directionsRenderer);
+    });
+    
+}
+
+function calcularRota(directionsService, directionsRenderer){
+    const selectedMode = document.getElementById("mode").value;
+    
+    directionsService
+    .route({
+        origin: { lat: -9.953317514546832, lng:-67.86271998458022 },
+        destination: document.getElementById("to").value,
+
+        travelMode: google.maps.TravelMode[selectedMode],
+    })
+    .then((response) => {
+        directionsRenderer.setDirections(response);
+    })
+    .catch((e) => window.alert("essa requisição falhou."));
+
+}
