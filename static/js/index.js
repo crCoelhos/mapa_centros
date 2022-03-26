75e6af39a9d3105e1a59cbf3ceff6a95b1c9499b
crCoelhos <cristophersaar@gmail.com>
2022-03-25 22:21:55 -0500

    correção de tabela e função

--- static/js/index.js
+++ static/js/index.js
@@ -30,14 +30,48 @@ function calcularRota(directionsService, directionsRenderer){
     })
 
     //exceção
-    .catch((e) => window.alert("clique no botão do centro e selecione o modo de direção.")); 
+    .catch((e) => window.alert("Clique nas coordenadas do centro desejado e selecione o modo de direção")); 
 
 }
 
-
+//tentando fazer a rota ser traçada insta ao clicar no botão, sem a necessidade de clicar no modo novamente
+// const atualizar_modo = document.getElementById("andando");
 //função para jogar as coordenadas geradas no box de input de direção
 const saida_rota = document.getElementById("to"); 
 
-function rodar_direcao() {
-    saida_rota.value = document.getElementById("destino{{item.id}}").value; 
+function rodar_direcao1() {
+    saida_rota.value = document.getElementById("destino1").value; 
+    // atualizar_modo.value = document.getElementById("andando").value;
 }
+function rodar_direcao2() {
+    saida_rota.value = document.getElementById("destino2").value; 
+}
+function rodar_direcao3() {
+    saida_rota.value = document.getElementById("destino3").value; 
+}
+function rodar_direcao4() {
+    saida_rota.value = document.getElementById("destino4").value; 
+}
+//solução gambiarra pro problema que eu não consegui resolver por completo:
+// contar quantidade de centros atuais e liberar escopo para inserção ou resolver o caso do recolhimento de valor por um elemento gerado com django
+// function rodar_direcao5() {
+//     saida_rota.value = document.getElementById("destino4").value; 
+  
+//     console.log(saida_rota)
+// }
+// function rodar_direcao6() {
+//     saida_rota.value = document.getElementById("destino4").value; 
+  
+//     console.log(saida_rota)
+// }
+
+// function rodar_direcao7() {
+//     saida_rota.value = document.getElementById("destino4").value; 
+  
+//     console.log(saida_rota)
+// }
+// function rodar_direcao8() {
+//     saida_rota.value = document.getElementById("destino4").value; 
+  
+//     console.log(saida_rota)
+// }
